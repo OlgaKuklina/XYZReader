1525db436e1f2d0defe7eae6050321168516b8ae
OlgaKuklina <okuklina@hotmail.com>
2016-05-30 20:30:50 -0700

    - implemented new ui based on CoordinatorLayout

--- app/src/main/java/com/example/android/xyzreader/activity/ListItemsActivity.java
+++ app/src/main/java/com/example/android/xyzreader/activity/ListItemsActivity.java
@@ -1,5 +1,9 @@
 package com.example.android.xyzreader.activity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.LoaderManager;
@@ -7,25 +11,43 @@ import android.support.v4.content.Loader;
 import android.support.v4.view.ViewCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBar;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.example.android.xyzreader.R;
 import com.example.android.xyzreader.adapter.ListItemsAdapter;
+import com.example.android.xyzreader.data.ArticleLoader;
+import com.example.android.xyzreader.data.UpdaterService;
 
 /**
  * Created by olgakuklina on 2016-05-04.
  */
 public class ListItemsActivity extends MainActivity implements LoaderManager.LoaderCallbacks<Cursor>, SwipeRefreshLayout.OnRefreshListener, ListItemsAdapter.OnListItemClickListener {
-
+    private static final String TAG = ListItemsActivity.class.getSimpleName();
     private RecyclerView.LayoutManager mLayoutManager;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView mRecyclerView;
     private SwipeRefreshLayout mSwipeRefreshLayout;
     private Toolbar mToolbar;
+    private boolean mIsRefreshing = false;
 
+    private BroadcastReceiver mRefreshingReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (UpdaterService.BROADCAST_ACTION_STATE_CHANGE.equals(intent.getAction())) {
+                mIsRefreshing = intent.getBooleanExtra(UpdaterService.EXTRA_REFRESHING, false);
+                updateRefreshingUI();
+            }
+        }
+    };
+
+    private void updateRefreshingUI() {
+        mSwipeRefreshLayout.setRefreshing(mIsRefreshing);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,9 +56,11 @@ public class ListItemsActivity extends MainActivity implements LoaderManager.Loa
         mRecyclerView = (RecyclerView) findViewById(R.id.items_list_recycler_view);
         mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
         ViewCompat.setElevation(mToolbar, getResources().getDimension(R.dimen.tb_elevation));
+
+
+
+
        // setSupportActionBar(mToolbar);
-
-
 
         ActionBar ab = getSupportActionBar();
         if (ab != null) ab.setTitle("");
@@ -46,8 +70,7 @@ public class ListItemsActivity extends MainActivity implements LoaderManager.Loa
 
         getSupportLoaderManager().initLoader(0, null, this);
 
-        if (savedInstanceState == null) {
-            onRefresh();
+        if (savedInstanceState == null) {refresh();
         }
     }
 
@@ -70,12 +93,26 @@ public class ListItemsActivity extends MainActivity implements LoaderManager.Loa
     }
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        return null;
+
+        Log.v(TAG, "onCreateLoader");
+        return ArticleLoader.newAllArticlesInstance(this);
     }
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        if(data == null) {
+            Log.v(TAG, "onLoadFinished null");
+        }
+        else {
+            Log.v(TAG, "onLoadFinished " + data.getCount());
+        }
 
+        mAdapter = new ListItemsAdapter(this,data).setListener(this);
+        mAdapter.setHasStableIds(true);
+        mRecyclerView.setAdapter(mAdapter);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
     }
 
     @Override
@@ -86,6 +123,22 @@ public class ListItemsActivity extends MainActivity implements LoaderManager.Loa
     @Override
     public void onListItemSelected(long articleId) {
 
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        registerReceiver(mRefreshingReceiver,
+                new IntentFilter(UpdaterService.BROADCAST_ACTION_STATE_CHANGE));
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(mRefreshingReceiver);
+    }
+
+    public void refresh() {
+        startService(new Intent(this, UpdaterService.class));
     }
 
     @Override
--- app/src/main/java/com/example/android/xyzreader/adapter/ListItemsAdapter.java
+++ app/src/main/java/com/example/android/xyzreader/adapter/ListItemsAdapter.java
@@ -4,18 +4,26 @@ import android.app.Activity;
 import android.database.Cursor;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.text.format.DateUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.android.xyzreader.R;
+import com.example.android.xyzreader.activity.ListItemsActivity;
 import com.example.android.xyzreader.data.ArticleLoader;
+import com.example.android.xyzreader.ui.DynamicHeightNetworkImageView;
+import com.example.android.xyzreader.ui.ImageLoaderHelper;
 
 /**
  * Created by olgakuklina on 2016-05-04.
  */
 public class ListItemsAdapter extends RecyclerView.Adapter<ListItemsAdapter.ViewHolder> {
+
+    public static final String TAG = ListItemsAdapter.class.getSimpleName();
+
     public ListItemsAdapter(Activity activity, Cursor cursor) {
 
         mActivity = activity;
@@ -41,43 +49,60 @@ public class ListItemsAdapter extends RecyclerView.Adapter<ListItemsAdapter.View
     }
     @Override
     public long getItemId(int position) {
+        Log.v(TAG, "getItemId = " + position );
         mCursor.moveToPosition(position);
         return mCursor.getLong(ArticleLoader.Query._ID);
     }
 
         public static class ViewHolder extends RecyclerView.ViewHolder {
             // each data item is just a string in this case
-
+            public DynamicHeightNetworkImageView thumbnailView;
             public TextView titleView;
             public TextView subtitleView;
 
             public ViewHolder(View v) {
                 super(v);
+                thumbnailView = (DynamicHeightNetworkImageView) v.findViewById(R.id.thumbnail);
                 titleView = (TextView) v.findViewById(R.id.article_title);
                 subtitleView = (TextView) v.findViewById(R.id.article_subtitle);
             }
         }
 
-
     @Override
     public ListItemsAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        Log.v(TAG, "ListItemsAdapter::onCreateViewHolder");
 
         View view =  LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
 
         final ViewHolder vh = new ViewHolder(view);
 
         return vh;
-
-
     }
 
     @Override
     public void onBindViewHolder(ListItemsAdapter.ViewHolder holder, int position) {
 
+        mCursor.moveToPosition(position);
+        holder.titleView.setText(mCursor.getString(ArticleLoader.Query.TITLE));
+        holder.subtitleView.setText(
+                DateUtils.getRelativeTimeSpanString(
+                        mCursor.getLong(ArticleLoader.Query.PUBLISHED_DATE),
+                        System.currentTimeMillis(), DateUtils.HOUR_IN_MILLIS,
+                        DateUtils.FORMAT_ABBREV_ALL).toString()
+                        + " by "
+                        + mCursor.getString(ArticleLoader.Query.AUTHOR));
+
+
+        holder.thumbnailView.setImageUrl(
+                mCursor.getString(ArticleLoader.Query.THUMB_URL),
+                ImageLoaderHelper.getInstance(mActivity).getImageLoader());
+        holder.thumbnailView.setAspectRatio(mCursor.getFloat(ArticleLoader.Query.ASPECT_RATIO));
+
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mCursor.getCount();
     }
 }
